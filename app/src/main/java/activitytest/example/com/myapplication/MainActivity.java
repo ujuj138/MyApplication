563b8a1a0e61a46f280242c46861e11a7ec23e86
liwenxuan <1366291049@qq.com>
2019-03-17 23:08:08 +0800

    提交文件

---
+++ app/src/main/java/activitytest/example/com/myapplication/MainActivity.java
@@ -0,0 +1,119 @@
+package activitytest.example.com.myapplication;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.media.MediaPlayer;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    ListView mylist;
+    List<Song> list;
+    MediaPlayer mediaPlayer=new MediaPlayer();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mylist = (ListView) findViewById(R.id.mylist);
+
+        list = new ArrayList<>();
+
+        list = MusicUtils.getmusic(this);
+
+        MyAdapter myAdapter = new MyAdapter(this, list);
+        mylist.setAdapter(myAdapter);
+
+
+    }
+
+    public void  play(String path){
+        try{
+            mediaPlayer.reset();
+
+            mediaPlayer.setDataSource(path);
+
+            mediaPlayer.setOnPreparedListener(new);
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+    class MyAdapter extends BaseAdapter {
+
+        Context context;
+        List<Song> list;
+
+        public MyAdapter(MainActivity mainActivity, List<Song> list) {
+            this.context = mainActivity;
+            this.list = list;
+        }
+
+
+        @Override
+        public int getCount() {
+            return list.size();
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return list.get(i);
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return i;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+
+            Myholder myholder;
+
+            if (view == null) {
+                myholder = new Myholder();
+                view = LayoutInflater.from(getApplicationContext()).inflate(R.layout.title, null);
+
+
+
+                myholder.t_position=(TextView) view.findViewById(R.id.t_postion);
+                myholder.t_song = (TextView)view.findViewById(R.id.t_song);
+                myholder.t_singer = (TextView)view.findViewById(R.id.t_singer);
+                myholder.t_duration = (TextView)view.findViewById(R.id.t_duration);
+
+                view.setTag(myholder);
+
+            } else {
+                myholder = (Myholder) view.getTag();
+            }
+
+            myholder.t_song.setText(list.get(i).song.toString());
+            myholder.t_singer.setText(list.get(i).singer.toString());
+            String time = MusicUtils.formatTime(list.get(i).duration);
+
+            myholder.t_duration.setText(time);
+            myholder.t_position.setText(i + 1 + "");
+
+
+            return view;
+        }
+
+
+        class Myholder {
+            TextView t_position, t_song, t_singer, t_duration;
+        }
+
+
+    }
+}
---
+++ app/src/main/java/activitytest/example/com/myapplication/Song.java
@@ -0,0 +1,13 @@
+package activitytest.example.com.myapplication;
+
+/**
+ * Created by liwenxuan on 2019/3/17.
+ */
+
+public class Song {
+    public String  song;
+    public String singer;
+    public long size;
+    public int duration;
+    public String path;
+}
